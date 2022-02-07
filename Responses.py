599dcabd1b74a13b0ff050a10d6a0cc559db6cb4
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-07 21:43:32 +0530

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("hello", "hi", "start"):
+    if user_message in ("hello", "hi", "start"):  # Where You Can Edit How The Bot Respond to User Input
         return "Hey! How's it going?"
 
     if user_message in ("who are you", "who are you?"):
